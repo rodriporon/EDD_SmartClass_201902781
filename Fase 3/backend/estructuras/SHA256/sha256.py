7c353e4c67e3a94edd4c15652fe9dbe890670e4e
rodriporon <3065713820401@ingenieria.usac.edu.gt>
2021-11-06 21:30:13 -0600

    se cambio correctamente el algoritmo sha256 para encriptar password

--- Fase 3/backend/estructuras/SHA256/sha256.py
+++ Fase 3/backend/estructuras/SHA256/sha256.py
@@ -1,14 +1,7 @@
 import hashlib
-import re 
 
 def generarHash(cadena):
-    patron = r'0000[0-9a-zA-Z]+'
-    nonce = 0
-    
-    while True:
-        suma = cadena + bytes(nonce)
-        id_hash =  hashlib.sha256(suma).hexdigest()
-        nonce +=1
-        if re.match(patron, id_hash):
-            return id_hash
+    m = hashlib.sha256()
+    m.update(cadena)
+    return (m.hexdigest())
         
